38a97adde5723534f04d58ed7dbbe6fd29efd9a5
Ujjwal Singh <ujjwalsingh0013@gmail.com>
2022-12-01 19:13:49 +0530

    added new text

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
+
 function App() {
   return (
     <CartProvider>
+ Here you are in main branch
+ 
       <Header />
       <main>
         <Meals />
